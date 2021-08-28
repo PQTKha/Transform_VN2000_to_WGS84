09d27b2e3c1c8c473bfa57eba669a83b3a7d5a7b
PQTKha <50334955+PQTKha@users.noreply.github.com>
2021-08-28 19:23:46 +0700

    Update variable names.py

--- variable names.py
+++ variable names.py
@@ -20,4 +20,11 @@ my-var ="John" #varibale has hyphen character
 my var ="John" #variable has space
 
 #Multi words variable names
-#variable 
+#variable names with more than one word can be difficult to read
+#There are several techniques you can use to make them more readable
+#Camel case: Each word, except the first, starts with a capital letter
+myVariableName ="John"
+#Pascal case: Each word starts with a capital letter
+MyVariableName ="John"
+#Snake case: Each word is separated by an underscore character:
+my_variable_name = "John"
