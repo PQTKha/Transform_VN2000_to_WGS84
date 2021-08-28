c45f524cbd21f1b922d95a3c87c7f2aec92083d1
PQTKha <50334955+PQTKha@users.noreply.github.com>
2021-08-28 19:15:43 +0700

    variable_name.py

---
+++ variable names.py
@@ -0,0 +1,23 @@
+#Python - variable names
+#A Variable can have a short name (like x and y) or more descriptive name (age, carname, total_volume)
+#Rules for python variable names:
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha_numeric characters and underscores (A-z, 0-9 and _)
+#A variable name are case-sensitive (age, Age and AGE are three different variables)
+
+#Example: legal variables:
+myvar = "John"
+my_var="John"
+_my_var = "John"
+myVar ="John"
+MYVAR ="John"
+myvar2 ="John"
+
+#Illegal variable names:
+2myvar = "John" #variable name start by number
+my-var ="John" #varibale has hyphen character 
+my var ="John" #variable has space
+
+#Multi words variable names
+#variable 
